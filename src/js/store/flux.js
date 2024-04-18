b93ed1b9f1566e899b5fc318eb8a80bcd464629e
dedalovitor <dedalovitor@gmail.com>
2024-04-18 16:00:13 +0000

    changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,40 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			locations: [],
+			episodes: [],
+
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			getCharacters: async () => {
+				const response = await fetch("https://rickandmortyapi.com/api/character");
+				const data = await response.json();
+				setStore({ characters: data.results })
+				console.log(data);
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+
+			getLocations: async () => {
+				const response = await fetch("https://rickandmortyapi.com/api/location");
+				const data = await response.json();
+				setStore({ locations: data.results });
+				console.log(data);
+			},
+
+			getEpisodes: async () => {
+				const response = await fetch("https://rickandmortyapi.com/api/episode");
+				const data = await response.json();
+				setStore({ episodes: data.results })
+				console.log(data);
+
+			},
+
 		}
-	};
+	}
 };
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,86 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect(() => {
+		actions.getCharacters();
+		actions.getLocations();
+		actions.getEpisodes();
+	}, [])
+
+	return (
+		< div className="text-center mt-5" >
+			<h1 className="mt-5">Characters</h1>
+			<div className="container" >
+				<div className="row">
+					{store.characters.map((item) => {
+						return (
+							<div className="card m-2 col-2" key={item.id}>
+								<img src={item.image} className="card-img-top" alt="..." />
+								<div className="card-body">
+									<h5 className="card-title">{item.name}</h5>
+									<p className="card-text">Species: {item.species}</p>
+									<p className="card-text">Gender: {item.gender}</p>
+									<p className="card-text">Status: {item.status}</p>
+									<a href="#" className="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						)
+					})
+
+					}
+
+
+				</div>
+				<h1 className="mt-5">Locations</h1>
+				<div className="container mt-5">
+					<div className="row">
+						{store.locations.map((item) => {
+							return (
+								<div className="card m-2" key={item.id} style={{ width: "18rem" }}>
+									<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMfsOio-gsDrxwPoJz51NqAFOrb2hOkWt4vQ&s" className="card-img-top" alt="..." />
+									<div className="card-body">
+										<h5 className="card-title">{item.name}</h5>
+										<p className="card-text">Type: {item.type}</p>
+										<p className="card-text">Dimension: {item.dimension}</p>
+										<p className="card-text">Residents: {item.residents.length}</p>
+										<a href="#" className="btn btn-primary">Go somewhere</a>
+									</div>
+								</div>
+							)
+						})
+						}
+					</div>
+				</div>
+
+				<h1 className="mt-5">Episodes</h1>
+				<div className="container mt-5">
+					<div className="row">
+						{store.locations.map((item) => {
+							return (
+								<div className="card m-2" key={item.id} style={{ width: "18rem" }}>
+									<img src="https://www.latercera.com/resizer/oStaaBjKjrTK8rrWE2MFitEba_g=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/FIN7ZFLY2VF7ZIO3F4VAH7DJ5I.jpg" className="card-img-top" alt="..." />
+									<div className="card-body">
+										<h5 className="card-title">{item.name}</h5>
+										<p className="card-text">Type: {item.type}</p>
+										<p className="card-text">Dimension: {item.dimension}</p>
+										<p className="card-text">Residents: {item.residents.length}</p>
+										<a href="#" className="btn btn-primary">Go somewhere</a>
+									</div>
+								</div>
+							)
+						})
+						}
+					</div>
+				</div>
+
+
+			</div>
+
+
+		</div >
+	);
+};
